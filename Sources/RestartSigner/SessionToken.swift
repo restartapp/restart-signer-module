7d39b70d745ebc74d9a4827609dc50e8e2e18ddd
Oscar Antonio Duran Grillo <hi@oduran.me>
2022-03-12 18:40:06 +0100

    Get key from environment variable

--- Sources/RestartSigner/SessionToken.swift
+++ Sources/RestartSigner/SessionToken.swift
@@ -30,7 +30,10 @@ public struct SessionToken: Content, Authenticatable, JWTPayload {
 }
 
 public enum Signer {
-  public static func registerDefault(key: String, _ app: Application) {
+  public static func registerDefault(_ app: Application) {
+    guard let key = Environment.get("SIGNER_KEY") else {
+      fatalError("Signer key was not set, check 'SIGNER_KEY' env variable")
+    }
     app.jwt.signers.use(.hs256(key: key), kid: .sessionToken)
   }
 }
